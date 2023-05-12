d6acbe3f8f8eda147bf4a253654dcd60c1d72532
SrishtiGautam <gautamsrishti757@gmail.com>
2023-05-13 02:57:42 +0530

    alphabet pyramid using for loop

---
+++ pattern.java
@@ -0,0 +1,14 @@
+public class pattern {
+    public static void main(String[]args){
+        char a='A';
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(a);
+                a++;
+            }
+            System.out.println();
+            a='A';
+        }
+    }
+    
+}
