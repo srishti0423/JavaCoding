baf81ebc7d8b942f8dc80996acddbfd97ae0f5e5
SrishtiGautam <gautamsrishti757@gmail.com>
2023-05-14 21:50:45 +0530

    made some changes in pyramid.java file

--- pyramid.java
+++ pyramid.java
@@ -2,7 +2,7 @@ public class pyramid {
     public static void main (String[]args){
         for(int i=1; i<=5;i++){
             for (int j=1;j<=i;j++){
-                System.out.print(1);
+                System.out.print(5);
             }
             System.out.println();
         }
